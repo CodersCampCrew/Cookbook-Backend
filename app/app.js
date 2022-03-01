bcda6b9d379b1efedad6974ff495ae24dce57e13
Marcin <marcinkukulka@gmail.com>
2022-03-01 20:07:00 +0100

    Add api endpoints

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
-const app = express();
 import routes from './routes/api.js'
-
 import './db/mongoose.js';
 
+const app = express();
+
+app.use(express.json())
 app.use('/api', routes);
 
 export default app;
